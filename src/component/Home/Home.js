da1ec12f8d2a3467a7d6c22b941a35790f03e0cc
Md: Mohon <mdmohon6145@gmail.com>
2022-06-24 21:36:21 +0600

    some change

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Loader from "../Loader/Loader";
 
 const Home = () => {
@@ -6,25 +7,36 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [searchItem, setSearchItem] = useState([]);
 
+  const navigate = useNavigate();
+
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon")
+    setLoading(true);
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchItem}`)
       .then((res) => res.json())
       .then((data) => {
-        setResult(data.results);
+        console.log(data);
+        setResult(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log("Something Wrong!");
+        navigate("/");
         setLoading(false);
       });
   }, [searchItem]);
 
-  const onChangeHandler = (event) => {
-    const eventValue = event.target.value;
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
 
-    const newData = result.filter((data) =>
-      data.name.toLocaleLowerCase().includes(eventValue.toLocaleLowerCase())
-    );
-    setSearchItem(newData);
+    const eventValue = event.target.search.value;
+    setSearchItem(eventValue);
+    console.log(eventValue);
+
+    // const newData = result.filter((data) =>
+    //   data.name.toLocaleLowerCase().includes(eventValue.toLocaleLowerCase())
+    // );
+    // setSearchItem(eventValue);
   };
-
-  console.log(searchItem);
 
   let contain;
   if (loading) {
@@ -33,32 +45,16 @@ const Home = () => {
     contain = (
       <>
         <div>
-          <div className="search-box">
-            <form>
-              <input
-                className="search-input px-10 py-3 rounded-md"
-                onChange={(event) => onChangeHandler(event)}
-                type="search"
-                name="search"
-                id=""
-                placeholder="search your item name"
-              />
-            </form>
-          </div>
-          <hr className="hr-style my-5" />
-
           <div className="item-section">
             <div className="grid gap-4 grid-cols-3 grid-rows">
-              {searchItem.map((item, index) => (
-                <div
-                  className="card bg-slate-100 rounded-md p-5 shadow-lg"
-                  key={index}
-                >
-                  <h1 className="font-bold">Name: {item.name}</h1>
-                  <p>Link: {item.url}</p>
-                  <p>There is no more data in this API</p>
+              {searchItem.length !== 0 ? (
+                <div className="card bg-slate-100 rounded-md p-5 shadow-lg text-center">
+                  {/* <img src={result.sprites.back_default} alt="" /> */}
+                  <h1 className="font-bold">Name: {result.name}</h1>
+                  <p>Height: {result.height}</p>
+                  <p>Weight: {result.weight}</p>
                 </div>
-              ))}
+              ) : null}
             </div>
             {searchItem.length === 0 ? (
               <div className="errorcheaker">
@@ -72,7 +68,29 @@ const Home = () => {
     );
   }
 
-  return <div>{contain}</div>;
+  return (
+    <div>
+      <div className="search-box">
+        <form onSubmit={(event) => onSubmitHandler(event)}>
+          <input
+            className="search-input px-10 py-3 rounded-md"
+            type="search"
+            name="search"
+            id=""
+            placeholder="search your item name"
+          />
+          <input
+            className="search-btn ml-3 px-5 py-3 rounded-md"
+            type="submit"
+            value="Search"
+          />
+        </form>
+      </div>
+      <hr className="hr-style my-5" />
+
+      {contain}
+    </div>
+  );
 };
 
 export default Home;
